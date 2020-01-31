e04fc7ec3c153729b0e37fdef9e2dda65e382783
Sorgfalt <yeongjun1324@gmail.com>
2020-01-31 16:13:17 +0900

    enum class 수정

--- src/com/company/NoteProgram.kt
+++ src/com/company/NoteProgram.kt
@@ -3,31 +3,28 @@ package com.company
 import com.sun.javafx.tools.packager.CreateBSSParams
 import java.util.*
 
-fun main() {
-    val Create : String = "Create"
-    val Read : String = "Read"
-    val Update : String = "Update"
-    val Delete : String = "Delete"
+enum class Note { CREATE, READ, UPDATE, DELETE }
+
+fun main(args: Array<String>) {
+    getNote(Note.CREATE)
+    getNote(Note.READ)
+    getNote(Note.UPDATE)
+    getNote(Note.DELETE)
+}
+
+fun getNote(n: Note) {
     val scanner = Scanner(System.`in`)
     println("프로그램을 시작합니다")
-        println("무엇을 입력하시겠습니까?" + "C. 생성" + "R. 읽기" + "U. 수정" + "D. 삭제")
-        println("입력값 : ")
-        val crud = scanner.nextLine()
-    when (crud) {
-            "C" -> print("값을 입력해주세요")
-            "R" -> print("몇번을 출력하시겠습니까?")
-            "U" -> print("몇번을 수정하시겠습니까?")
-            "D" -> print("몇번을 삭제하시겠습니까?")
-            else -> print("값을 다시 입력해주세요")
-
+    println("무엇을 입력하시겠습니까?" + "C. 생성 " + "R. 읽기 " + "U. 수정 " + "D. 삭제")
+    println("입력값 : ")
+    val sc = scanner.nextLine()
+    when (n) {
+        Note.CREATE -> print("값을 입력해주세요 ")
+        Note.READ -> print("몇번을 출력하시겠습니까?")
+        Note.UPDATE -> print("몇번을 수정하시겠습니까?")
+        Note.DELETE -> print("몇번을 삭제하시겠습니까?")
+        else -> print("값을 다시 입력해주세요!")
     }
-    var userName : String = scanner.nextLine()
-    println(userName = User(userName))
-    println(userName.hashCode())
-    println(userName)
-}
-data class User(var name : String){
-
 }
 
 
